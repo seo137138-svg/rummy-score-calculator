6524b8c76ee8cf8fd65612edca762a1966750ee2
Rummy Nobel <seo137138@gmail.com>
2026-06-02 10:11:47 +0800

    Update probability_engine.py

--- probability_engine.py
+++ probability_engine.py
@@ -1,17 +1,1 @@
-def sequence_potential(hand):
-    """
-    Estimate number of potential sequences in a hand
-    """
-    from itertools import combinations
-    potential = 0
-    # all 3cards
-    for combo in combinations(hand, 3):
-        ranks = [c[:-1] for c in combo]
-        if len(set(ranks)) == 3: 
-            potential +=1
-    return potential
 
-
-if __name__ == "__main__":
-    hand = ['7H','8H','9H','KS','KD','KC']
-    print("Sequence Potential:", sequence_potential(hand))
